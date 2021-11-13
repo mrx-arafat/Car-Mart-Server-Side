8e14a60dcbecbe162830635853e7a77dfe5e1b39
Easin Arafat <arafatmrx@gmail.com>
2021-11-13 19:12:05 +0600

    connecting db

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require("cors");
+app.use(cors());
+
+require("dotenv").config();
+
+//connect
+
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+console.log(uri);
+
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello car mart");
 });
 
 app.listen(port, () => {
